d8696cac7bbdd8aa5a2a7529aecd408aef57d46e
AnChatzig <anchatzigiannakis@gmail.com>
2026-01-28 20:43:58 +0200

    feat(19-04): export auto_eq functions from analysis module
    - Add imports for get_target_curve, calculate_eq_bands, analyze_auto_eq
    - Update __all__ to export all analysis functions

--- python/mic_eq/analysis/__init__.py
+++ python/mic_eq/analysis/__init__.py
@@ -10,10 +10,20 @@ from .spectrum import (
     smooth_spectrum_octave,
     find_octave_spaced_peaks
 )
+from .auto_eq import (
+    get_target_curve,
+    calculate_eq_bands,
+    analyze_auto_eq
+)
 
 __all__ = [
+    # Spectrum analysis
     'compute_voice_spectrum',
     'get_octave_frequencies',
     'smooth_spectrum_octave',
-    'find_octave_spaced_peaks'
+    'find_octave_spaced_peaks',
+    # EQ calculation
+    'get_target_curve',
+    'calculate_eq_bands',
+    'analyze_auto_eq'
 ]
